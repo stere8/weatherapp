e3143fd1b30dc0c61c4b182e2c60006d3f822585
stere8 <stere8@github.com>
2020-02-16 19:36:42 +0100

    Few Adjustments

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r##1l_$=_rt!(gljybnsdrx%@n1p=0@_^v*b+p%dy%a3as*k69'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
